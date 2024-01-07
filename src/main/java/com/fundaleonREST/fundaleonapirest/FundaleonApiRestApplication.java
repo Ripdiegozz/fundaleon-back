70ab2b6af44545fc67c1f99435967e429762a434
Diego Garcia <dagadev@proton.me>
2024-01-07 00:20:21 -0500

    Testing heroku deployment

--- src/main/java/com/fundaleonREST/fundaleonapirest/FundaleonApiRestApplication.java
+++ src/main/java/com/fundaleonREST/fundaleonapirest/FundaleonApiRestApplication.java
@@ -1,13 +1,18 @@
 package com.fundaleonREST.fundaleonapirest;
-
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class FundaleonApiRestApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(FundaleonApiRestApplication.class, args);
 	}
 
+	@Bean
+	public Dotenv dotenv() {
+		return Dotenv.configure().load();
+	}
 }
